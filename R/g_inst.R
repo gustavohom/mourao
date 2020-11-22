f7658644e0b5c243e77666442991dd99686b1ef6
Gustavo Mourao <gustavohen1@gmail.com>
2020-11-22 12:52:12 -0300

    asdasd

--- R/g_inst.R
+++ R/g_inst.R
@@ -1,4 +1,21 @@
-pkg <-function(pkg) {
+#' @title Carregar
+#' @name inst
+#'
+#' @description nao quero
+#'
+#' @param pkg teste
+#'
+#' @details Depois detalho
+#'
+#' @return X
+#'
+#' @author Mourao
+#'
+#' @examples
+#' asd
+#' @export
+#' 
+inst <-function(pkg) {
   
   ipkg <- c(pkg[!pkg %in% installed.packages()], 
             pkg[pkg %in% rownames(old.packages())])
