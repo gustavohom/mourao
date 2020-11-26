0cbb9a37395f02436759e9285a96d04a721df0c0
Gustavo Mourao <gustavohen1@gmail.com>
2020-11-26 15:03:14 -0300

    a

--- R/m_load.R
+++ R/m_load.R
@@ -1,14 +1,14 @@
-#' @title Carregar, instalar e atualizar pacotes
+#' @title Carregar pacotes
+#' 
 #' @name m_load
 #'
-#' @description A função verifica se o pacote está instalado e atualizado. Caso negativo, irá instalar a versão mais atualizada do pacote.
-#' Em seguida, carrega todos os pacotes nomeados.
+#' @description Carregar, instalar e atualizar pacotes
 #'
 #' @param pkg o nome de um pacote ou um vetor de string com o nome de varios pacotes.
 #'
-#' @details A função verifica se o pacote está instalado e atualizado. Caso negativo, irá instalar a versão mais atualizada do pacote.
+#' @details A funcao verifica se o pacote esta instalado e atualizado. Caso negativo, instalara a versao mais atualizada do pacote.
 #' Em seguida, carrega todos os pacotes nomeados.
-#'
+#' 
 #' @return Pacotes anexados
 #'
 #' @author Mourao
